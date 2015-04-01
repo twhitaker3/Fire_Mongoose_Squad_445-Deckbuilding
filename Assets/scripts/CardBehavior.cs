e5476c6ea5c22785fd3cac165a515f810f951c47
thomas2015 <thomas.mulholland.2015@gmail.com>
2015-04-01 11:03:04 -0500

    Adding scripts

---
+++ Assets/scripts/CardBehavior.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardBehavior : MonoBehaviour {
+
+	public int state;
+	public int index;
+	public Sprite back_face;
+	public Sprite[] faces;
+
+	SpriteRenderer spriteRenderer;
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void ShowBack(){
+		spriteRenderer.sprite = back_face;
+	}
+	public void ShowFront(){
+		spriteRenderer.sprite = faces [index];
+	}
+	public void OnMouseDown(){
+		switch (state) {
+		case 0:
+			ShowFront();
+			print("Front");
+			state = 1;
+			break;
+		case 1:
+			ShowBack();
+			print ("Back");
+			state = 0;
+			break;
+		}
+	}
+}
---
+++ Assets/scripts/PlayedZoneBehavior.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayedZoneBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
