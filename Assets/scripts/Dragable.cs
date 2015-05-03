37136024e400061601621714a501cb078af714ff
twhitaker3 <twhitaker3@tamu.edu>
2015-05-03 12:11:34 -0500

    draggable interface
    testing done in scene draggable

---
+++ Assets/scripts/Dragable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class Dragable : MonoBehaviour, IDragHandler, IEndDragHandler, IBeginDragHandler {
+
+	public Transform parentToReturnTo = null;
+
+	public void OnBeginDrag(PointerEventData data){
+		Debug.Log ("on begin drag");
+		parentToReturnTo = this.transform.parent;
+		this.transform.SetParent(this.transform.parent.parent);
+		GetComponent<CanvasGroup> ().blocksRaycasts = false;
+	}
+	public void OnDrag(PointerEventData data){
+		//Debug.Log ("on drag");
+		this.transform.position = data.position;
+	}
+	public void OnEndDrag(PointerEventData data){
+		Debug.Log ("on end drag");
+		this.transform.SetParent (parentToReturnTo);
+		GetComponent<CanvasGroup> ().blocksRaycasts = true;
+	}
+
+}
---
+++ Assets/scripts/DropZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems; 
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter(PointerEventData data){
+		//Debug.Log ("OnPointerEnter");
+	}
+	public void OnDrop(PointerEventData data){
+		Debug.Log ("OnDrop to " + gameObject.name);
+		Dragable d = data.pointerDrag.GetComponent<Dragable>();
+		if (d != null) {
+			d.parentToReturnTo = this.transform;
+		}
+	}
+	public void OnPointerExit(PointerEventData data){
+		//Debug.Log ("OnPointerExit");
+	}
+}
