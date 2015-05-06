008ba1acb3860b7da9b0a8f14ea9da5f1408f003
twhitaker3 <twhitaker3@tamu.edu>
2015-05-06 14:16:44 -0500

    Final presentation
    here is the build from the final presentation, THOMAS, if you want to
    work with the other scene its called test, the url for the videos i used
    is here: https://www.youtube.com/watch?v=bMuYUOIAdnc

---
+++ Assets/scripts/EndTurnButtonBehavior.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndTurnButtonBehavior : MonoBehaviour {
+
+	public GameObject Player1;
+	public GameObject Player2;
+	public GameObject line_up;
+	public GameObject villian;
+	public GameObject player1_Discard;
+	public GameObject player2_Discard;
+
+
+	public void switch_turns(){
+		if (Player1.GetComponent<PlayerBehavior> ().active) {
+			Player1.GetComponent<PlayerBehavior> ().active = false;
+			Player2.GetComponent<PlayerBehavior> ().active = true;
+			line_up.GetComponent<LineUpBehavior> ().player_discard = player2_Discard;
+			villian.GetComponent<VillainDeckBehavior> ().player_discard = player2_Discard;
+		} else {
+			Player2.GetComponent<PlayerBehavior> ().active = false;
+			Player1.GetComponent<PlayerBehavior> ().active = true;
+			line_up.GetComponent<LineUpBehavior> ().player_discard = player1_Discard;
+			villian.GetComponent<VillainDeckBehavior> ().player_discard = player1_Discard;
+
+		}
+	}
+}
